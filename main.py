0cbfa3d5ec0792bc21499fc6113d30c8f16e8491
Oleg Rukin <orukin@20thousandleagues.com>
2019-04-29 23:36:36 +0300

    use extractor from url body

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import google_auth_oauthlib.flow
 
+import extractor
 import playlist_creator
 import playlist_insert
 import video_finder
@@ -22,7 +23,7 @@ def main(url):
     credentials = flow.run_console()
 
     name = '55 Awesome Metal Covers of Classic Rock Songs'
-    result = ['Amorphis, Light My Fire (The Doors)', 'Anthrax, Carry on Wayward Son (Kansas)', 'Armored Saint, Saturday Night Special (Lynyrd Skynyrd)', 'Between the Buried and Me, Bicycle Race (Queen)', 'Black Sabbath, Evil Woman (Crow)', 'Blessthefall, Dream On (Aerosmith)', 'Body Count, Hey Joe (Billy Roberts)', 'Carnivore, Manic Depression (Jimi Hendrix)', 'Celtic Frost, Heroes (David Bowie)', 'Children of Bodom, Somebody Put Something in My Drink (Ramones)', 'Cirith Ungol, Fire (The Crazy World of Arthur Brown)', 'Crowbar, No Quarter (Led Zeppelin)', 'Dark Angel, Immigrant Song (Led Zeppelin)', 'Death, God of Thunder (KISS)', 'Destruction, My Sharona (The Knack)', 'Disturbed, The Sound of Silence (Simon and Garfunkel)', 'Dream Theater, Funeral for a Friend (Elton John)', 'Exodus, Low Rider (War)', 'Flotsam and Jetsam, Saturday Night’s Alright for Fighting (Elton John)', 'Fu Manchu, Godzilla (Blue Oyster Cult)', 'Ghost, Here Comes the Sun (The Beatles)', 'Guns N’ Roses, Knocking on Heaven’s Door (Bob Dylan)', 'GWAR, School’s Out (Alice Cooper)', 'Helloween, White Room (Cream)', 'HIM, (Don’t Fear) The Reaper (Blue Oyster Cult)', 'Iron Maiden, Cross-Eyed Mary (Jethro Tull)', 'Judas Priest, Green Manalishi (Fleetwood Mac)', 'Machine Head, Message in a Bottle (The Police)', 'Mastodon, Just Got Paid (ZZ Top)', 'Megadeth, No More Mr. Nice Guy (Alice Cooper)', 'Melvins, Goin’ Blind (KISS)', 'Metal Church, Highway Star (Deep Purple)', 'Metallica, Turn the Page (Bob Seger)', 'Monster Magnet, Brainstorm (Hawkwind)', 'Motley Crue, Smokin’ in the Boys Room (Brownsville Station)', 'Motorhead, Heroes (David Bowie)', 'Nightwish, High Hopes (Pink Floyd)', 'Overkill, Deuce (KISS)', 'Ozzy Osbourne, 21st Century Schizoid Man (King Crimson)', 'Primal Fear, Speed King (Deep Purple)', 'Queensryche, Welcome to the Machine (Pink Floyd)', 'Quiet Riot, Cum on Feel the Noize (Slade)', 'Riot, Born to Be Wild (Steppenwolf)', 'Rob Zombie/Marilyn Manson, Helter Skelter (The Beatles)', 'Sanctuary, White Rabbit (Jefferson Airplane)', 'Shadows Fall, Welcome to the Machine (Pink Floyd)', 'Slayer, In a Gadda Da Vida (Iron Butterfly)', 'Sodom, Surfin’ Bird (The Trashmen)', 'Testament, Nobody’s Fault (Aerosmith)', 'Trouble', 'Twisted Sister, It’s Only Rock and Roll (Rolling Stones)', 'Type O Negative, Cinnamon Girl (Neil Young)', 'Various Artists (Ronnie James Dio, Yngwie Malmsteen, Stu Hamm, Paul Taylor & Gregg Bisonette), Dream On (Aerosmith)', 'Voivod, Astronomy Domine (Pink Floyd)', 'W.A.S.P., The Real Me (The Who)']
+    result = extractor.extract(url)
     playlist_id = playlist_creator.create_playlist(name, credentials)
     for song in result:
         video_id = video_finder.find_video(song)
